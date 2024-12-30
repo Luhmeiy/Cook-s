bd04a71b22bb5ff7cc3ae3d9e070b5f137f2fbc7
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2024-12-30 01:16:21 -0300

    refactor(landing-page): remove footer

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,15 +1,22 @@
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import GlobalStyles from "@/GlobalStyles";
 import Header from "../Header";
 import Footer from "../Footer";
+import { selectCurrentUserId } from "@/features/auth/authSlice";
 
 const Layout = () => {
+	const userId = useSelector(selectCurrentUserId);
+	const isHome = location.pathname === "/";
+
+	const showFooter = !isHome || (userId && isHome);
+
 	return (
 		<>
 			<GlobalStyles />
 			<Header />
 			<Outlet />
-			<Footer />
+			{showFooter && <Footer />}
 		</>
 	);
 };
